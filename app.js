d7499bdadc57630151cdd137f5b49540414737d9
Tram Lai <tramlaisf@gmail.com>
2014-04-09 16:46:19 -0500

    Fix for heroku

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var server = http.createServer(app);
 var path = require('path');
 var fs = require('fs');
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.use(express.logger('dev'));
@@ -22,17 +22,12 @@ app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-};
-
-app.get('/', function(req,res){
-  res.render('index.html', {title: "Hello world"});
-});
-
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+// Heroku won't actually allow WebSockets
+// have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
 });
 
 var io = require('socket.io').listen(server);
